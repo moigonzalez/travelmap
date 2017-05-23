816d43d53ea270f7f3d9fcbed23ad14f881b8296
Moises Gonzalez <moises.gonzalez@netcentric.biz>
2017-05-23 16:57:21 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Travelmap from './travelmap';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Travelmap />, document.getElementById('root'));
+// registerServiceWorker();
